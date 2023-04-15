5efeab318163cf53b4445a5e04cbe8c07cc1c965
EduardoHGz <88729982+LaloHGz@users.noreply.github.com>
2023-04-15 12:51:59 -0600

    Se creo el proyecto base

---
+++ EmocionaMente/EmocionaMente/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EmocionaMente
+//
+//  Created by Eduardo Jair Hernández Gómez on 15/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmocionaMente/EmocionaMente/EmocionaMenteApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmocionaMenteApp.swift
+//  EmocionaMente
+//
+//  Created by Eduardo Jair Hernández Gómez on 15/04/23.
+//
+
+import SwiftUI
+
+@main
+struct EmocionaMenteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
