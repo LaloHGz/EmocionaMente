be0eb8bc56a8c49d94c3fb300c149bace6cda2f7
Carmina López <a00830265@itesm.mx>
2023-04-16 14:34:04 -0600

    Filtro

--- EmocionaMente/EmocionaMente/ContentView.swift
+++ EmocionaMente/EmocionaMente/ContentView.swift
@@ -7,31 +7,69 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
+
+var arView: ARView!
 
 struct ContentView : View {
+    
+    func TakeSnapshot() {
+      // 1
+      arView.snapshot(saveToHDR: false) { (image) in
+        // 2 Reduce the image size
+        let compressedImage = UIImage(
+          data: (image?.pngData())!)
+        // 3
+        UIImageWriteToSavedPhotosAlbum(
+          compressedImage!, nil, nil, nil)
+      }
+    }
+    
     var body: some View {
-        Text("Emociona Mente")
-        //ARViewContainer().edgesIgnoringSafeArea(.all)
+        //Text("Emociona Mente")
+        ARViewContainer().edgesIgnoringSafeArea(.all)
+          Button(action: {
+              self.TakeSnapshot()
+          }) {
+              Image(systemName: "trash")
+                      .font(.largeTitle)
+                      .foregroundColor(.red)
+                      .clipShape(Circle())
+          }
+
     }
 }
+
+// two-way connection between  the property that stores the data and the view that changes and displays the data.
+
 
 struct ARViewContainer: UIViewRepresentable {
     
     func makeUIView(context: Context) -> ARView {
         
-        let arView = ARView(frame: .zero)
+        arView = ARView(frame: .zero)
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        //let boxAnchor = try! Experience.loadBox()
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        //arView.scene.anchors.append(boxAnchor)
+        
+        let arAnchor = try! Experience.loadGlasses()
+        arView.scene.anchors.append(arAnchor)
         
         return arView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+        // 1
+        let arConfiguration = ARFaceTrackingConfiguration()
+        // 2
+        uiView.session.run(arConfiguration,
+          options:[.resetTracking, .removeExistingAnchors])
+
+    }
     
 }
 
